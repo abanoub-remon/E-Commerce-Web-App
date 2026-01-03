5f5d4aa24dc91472d350c0fce4a9a35e5919a8ac
abanoub-remon <abanoubremon512@gmail.com>
2026-01-03 20:27:11 +0200

    modify settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e$vq+h=9=k(&h19*$@!z*rpqz3nkv$1!mo^g@y&t6w4+qt+qtv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 from datetime import timedelta
 
