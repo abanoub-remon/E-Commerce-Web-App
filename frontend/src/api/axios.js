36ea87ba5513cec5d0aa8faedf67b6e2dd65dd7c
abanoub-remon <abanoubremon512@gmail.com>
2026-01-03 21:52:58 +0200

    api edit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:8000/api",
+	baseURL: "https://abanoubremon.pythonanywhere.com/api",
+	withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
